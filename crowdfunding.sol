a2036c65248267d9af7d391ced988438572f9c2a
GUNNADA MEGHANA <101832619+MeghanaGunnada@users.noreply.github.com>
2023-08-20 15:39:56 +0530

    Add files via upload

---
+++ crowdfunding.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Crowdfunding {
+    address public creator;
+    uint public goal;
+    uint public raisedAmount;
+    mapping(address => uint) public contributions;
+    bool public fundingClosed;
+
+    constructor(uint _goal) {
+        creator = msg.sender;
+        goal = _goal;
+    }
+
+    modifier onlyCreator() {
+        require(msg.sender == creator, "Only the creator can call this function");
+        _;
+    }
+
+    function contribute() external payable {
+        require(!fundingClosed, "Funding is closed");
+        contributions[msg.sender] += msg.value;
+        raisedAmount += msg.value;
+    }
+
+    function withdrawFunds() external onlyCreator {
+        require(raisedAmount >= goal, "Goal not reached");
+        payable(creator).transfer(raisedAmount);
+        fundingClosed = true;
+    }
+
+    function claimRefund() external {
+        require(fundingClosed && raisedAmount < goal, "Refunds are not available");
+        uint amount = contributions[msg.sender];
+        contributions[msg.sender] = 0;
+        payable(msg.sender).transfer(amount);
+    }
+}
